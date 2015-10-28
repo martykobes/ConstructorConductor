57b8976f0bf44022abc6686e0fc2cb4fe2d03112
Marty Kowbel <mekobes44@yahoo.com>
2015-10-28 15:41:33 -0500

    array prop assognment

--- arrayProperty.js
+++ arrayProperty.js
@@ -2,4 +2,9 @@
 
 //Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
   
-  //code here
+  String.prototype.reverse = function() {
+      str = str.split(' ');
+      str = str.reverse();
+      str = str.join(' ';)
+      return str;
+  }
